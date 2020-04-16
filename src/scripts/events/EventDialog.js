3ca529b5715ccf4e4f49577574663fdf4b57bb93
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-04-16 14:44:56 -0500

    removed form to prevent page reload

--- src/scripts/events/EventDialog.js
+++ src/scripts/events/EventDialog.js
@@ -3,6 +3,7 @@
 
 import { saveEvent } from "../events/EventProvider.js"
 
+
 const contentTarget = document.querySelector(".dialogsContainer")
 const eventHub = document.querySelector(".container")
 
@@ -14,26 +15,30 @@ eventHub.addEventListener("newEventBtnClicked", customEvent => {
 export const EventDialog = () => {
     contentTarget.innerHTML += `
         <dialog id="event__dialog">
-            <form class="event__form">
-                <fieldset>
-                    <label for="event--date">Date:</label>
-                    <input id="event--date" name="event--date" type="date"/>
-                </fieldset>
-                <fieldset>
-                    <label for="event--name">Name:</label>
-                    <input id="event--name" name="event--name" type="text"/>
-                </fieldset>
-                <fieldset>
-                    <label for="event--location">Location:</label>
-                    <input id="event--location" name="event--location" type="text"/>
-                </fieldset>
-                <button id="saveEventBtn">Save Event</button>
-            </form>
+            <fieldset>
+                <label for="event--date">Date:</label>
+                <input id="event--date" name="event--date" type="date"/>                
+            </fieldset>
+            <fieldset>
+                <label for="event--name">Name:</label>
+                <input id="event--name" name="event--name" type="text"/>
+            </fieldset>
+            <fieldset>
+                <label for="event--location">Location:</label>
+                <input id="event--location" name="event--location" type="text"/>
+            </fieldset>
+            <button id="saveEventBtn">Save Event</button>
         </dialog>
     `
 }
 
+const dispatchDialogClosedEvent = () => {
+    const dialogClosedEvent = new CustomEvent("dialogClosed")
+    eventHub.dispatchEvent(dialogClosedEvent)
+}
+
 contentTarget.addEventListener("click", clickEvent => {
+    const eventDialog = document.querySelector("#event__dialog")
     if (clickEvent.target.id === "saveEventBtn") {
         const eventDate = document.querySelector("#event--date").value 
         const eventName = document.querySelector("#event--name").value 
@@ -43,7 +48,12 @@ contentTarget.addEventListener("click", clickEvent => {
                 name: eventName,
                 location: eventLocation
             }
-        saveEvent(newEvent)
+        if (name !== "" && location !== "") {
+            saveEvent(newEvent)
+        } else {
+            alert("Please complete all fields before saving.")
+        }
         eventDialog.close()
-    }
+        dispatchDialogClosedEvent()
+    }  
 })
