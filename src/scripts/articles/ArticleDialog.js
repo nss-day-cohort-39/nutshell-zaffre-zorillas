df1e24ab39ed87eb5a5484676fd8db5d603f0854
kristen-howton <kristen.howton@gmail.com>
2020-04-16 09:34:10 -0500

    all article fields required to create a new article

--- src/scripts/articles/ArticleDialog.js
+++ src/scripts/articles/ArticleDialog.js
@@ -37,7 +37,13 @@ contentTarget.addEventListener("click", event => {
         document.querySelector("#article").value = ""
         const newArticle = articleFactory(title, synopsis, url, userId)
         
-        saveArticle(newArticle)
+        if (title !== "" && synopsis !== "" && url !== "") {
+            saveArticle(newArticle)
+        } else {
+            alert("Opps, it looks like you forgot to fill out all of the required fields.")
+        }
+
+        
         //Method that closes form dialog
         articleDialog.close()
     }
